caa68d786e85ef8b927df43ca30d68f239869000
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-03-11 18:49:46 +0200

    scripts: main, music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFX);
+    }
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFX);
+    }
+}
---
+++ Assets/Scripts/main_Menu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class main_Menu : MonoBehaviour
+{
+    public GameObject Main_Menu;
+
+    public static bool isOpened = false;
+
+    public void StartTheGame()
+    {
+        Debug.Log("Start");
+
+        SceneManager.LoadScene("Level_game");
+    }
+
+    public void TOPTheGame()
+    {
+
+        SceneManager.LoadScene("TopTable");
+    }
+    public void QuitTheGame()
+    {
+
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void Start()
+    {
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+    }
+
+
+    public void Update()
+    {
+
+
+
+    }
+
+}

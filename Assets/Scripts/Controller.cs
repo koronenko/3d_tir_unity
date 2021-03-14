3e1a3ab2bace0ed4e15a91d06a1393c9efc0a1c3
Anastasiia Koronenko <koronenko.nastya@gmail.com>
2021-03-14 16:17:58 +0200

    fire sysnem FX

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
 public class Controller : MonoBehaviour
 {
-    // екземпляр спавна
-    public GameObject pref;
-    public GameObject _target;
-    private GameObject inst_target;
-    public Camera cam;
+    // екземпляри спавна
+    public GameObject pref; //змінна префабу сфери
+    public GameObject _target; //змінна мішені кубу
+    private GameObject inst_target; //ініціалізація мішені
+    public AudioClip shot; //звук вистрілу
+    AudioSource audioSource;
+    public Camera cam; // головна камера
 
     private void Start()
     {
-        inst_target = Instantiate(_target, _target.transform.position, Quaternion.identity);
-        inst_target.transform.position = new Vector3(-9.82f, 4.22f, -6.32f);
+        inst_target = Instantiate(_target, _target.transform.position, Quaternion.identity); //вивід префабу
+        inst_target.transform.position = new Vector3(-9.82f, 4.22f, -6.32f); //точка спавна
+        audioSource = GetComponent<AudioSource>();
 
     }
     private void Update()
     {
-        Spawn();
+        Spawn(); //стрільба по мішені
+
 
     }
     public void Spawn()
@@ -23,13 +30,15 @@ public class Controller : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition); //в точці позиції курсору
 
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.transform.tag == "target")
                 {
-                    Instantiate(pref, hit.point, Quaternion.identity);
+                    Instantiate(pref, hit.point, Quaternion.identity); // відображаємо сферу
+                    audioSource.PlayOneShot(shot, 1f);
+
                 }
             }
         }
